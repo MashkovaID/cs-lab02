3fd73568d66df7dc2275f426337e5fb04adc402d
Alice(MashkovaID) <alice@example.com>
2019-03-05 16:55:51 +0300

    code:добавление максимума от имени Алисы

--- main.cpp
+++ main.cpp
@@ -11,5 +11,8 @@ int main()
     cout<< "A - B = " << a - b <<'\n'
     cout<< "A * B  " << a * b <<'\n'
     cout<< "A / B = " << a / b <<'\n'
+    cout<< " max = "
+    if(a>b) {cout<<a<<'\n'}
+    else {cout<<b<<'\n'}
     return 0;
 }
